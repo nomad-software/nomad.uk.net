aa49e0fcd8ba8ac8dc5e32d7178dfc9c8de62cc7
Gary Willoughby <dev@nomad.so>
2017-12-09 16:59:36 +0000

    Updated the container build.

--- build-container.sh
+++ build-container.sh
@@ -2,7 +2,7 @@
 
 ./build-site.sh;
 
-docker build -t "nomad.uk.net:latest" .;
+docker build -t "nomadsoftware/nomad.uk.net:latest" .;
 
 # Test running it.
-docker run --rm -i -t -p 80:80 nomad.uk.net:latest;
+# docker run --rm -i -t -p 80:80 nomad.uk.net:latest;
