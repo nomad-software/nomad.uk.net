b4998bcf125250d3bfc705b94718573a9d3f48e0
Gary Willoughby <dev@nomad.so>
2017-12-18 18:54:50 +0000

    Added optimisation tweaks.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM nginx:alpine
 VOLUME /var/log/nginx
+COPY nginx/nginx.conf /etc/nginx/nginx.conf
 COPY public /usr/share/nginx/html
 EXPOSE 80
--- build-site.sh
+++ build-site.sh
@@ -2,39 +2,29 @@
 
 find ./public -iname "*.html" -exec rm -v {} \;
 
-find ./markdown/articles -iname "*.md" -print0 | while read -d $'\0' file
+find ./markdown -iname "*.md" -print0 | while read -d $'\0' file
 do
 	LONG=$file;
 	SHORT=$(echo ${LONG} | sed -E "s/^\.\/markdown\/(.*)\.md/\1/");
 	URL="http://nomad.uk.net/${SHORT}.html";
 	ID=$(echo ${URL} | md5sum | cut -d ' ' -f 1);
 
+	mkdir -p ./public/$(dirname ${SHORT});
+
+	if [[ ${SHORT} =~ ^(index|pages/errors/404)$ ]]
+	then
+		pandoc --standalone \
+			--template="templates/default.html" \
+			--output="./public/${SHORT}.html" \
+			${LONG};
+	else
+		pandoc --standalone \
+			--template="templates/default.html" \
+			--output="./public/${SHORT}.html" \
+			--variable=disqus-url:${URL} \
+			--variable=disqus-id:${ID} \
+			${LONG};
+	fi;
+
 	echo "created './public/${SHORT}.html'";
-
-	pandoc --standalone \
-		--template="templates/default.html" \
-		--output="./public/${SHORT}.html" \
-		--variable=disqus-url:${URL} \
-		--variable=disqus-id:${ID} \
-		${LONG};
 done;
-
-find ./markdown/pages -iname "*.md" -print0 | while read -d $'\0' file
-do
-	LONG=$file;
-	SHORT=$(echo ${LONG} | sed -E "s/^\.\/markdown\/(.*)\.md/\1/");
-	URL="http://nomad.uk.net/${SHORT}.html";
-	ID=$(echo ${URL} | md5sum | cut -d ' ' -f 1);
-
-	echo "created './public/${SHORT}.html'";
-
-	pandoc --standalone \
-		--template="templates/default.html" \
-		--output="./public/${SHORT}.html" \
-		--variable=disqus-url:${URL} \
-		--variable=disqus-id:${ID} \
-		${LONG};
-done;
-
-pandoc --standalone --template="templates/default.html" --output="./public/index.html" "./markdown/index.md";
-echo "created './public/index.html'";
