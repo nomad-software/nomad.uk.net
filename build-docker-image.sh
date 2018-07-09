3fd904a3800823477258b8cdcdd19e8613131037
Gary Willoughby <dev@nomad.so>
2018-07-09 17:57:20 +0100

    Updated the docker build script.

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -9,5 +9,5 @@ echo "Building new docker image"
 docker build -t "nomadsoftware/nomad.uk.net:latest" .;
 docker images;
 
-echo -e "Run the following to test a container\n"
-echo -e "docker run --rm -i -t -p 80:80 nomadsoftware/nomad.uk.net:latest\n";
+echo "";
+echo "docker run --rm -i -t -p 80:80 nomadsoftware/nomad.uk.net:latest";
